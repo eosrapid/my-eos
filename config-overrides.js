a08bae89105d568c58c9aee5d88daf6a2b41b120
eosrapid <blocks4days@outlook.com>
2021-04-18 21:37:16 +0800

    fixed react scripts version

--- config-overrides.js
+++ config-overrides.js
@@ -9,6 +9,7 @@ function findAndFixCSSPath(config, newCssFilePath) {
   }
 }
 function override(config, env) {
+  console.log("eee")
   config.resolve.alias["react"] = "preact/compat";
   config.resolve.alias["react-dom/test-utils"] = "preact/test-utils";
   config.resolve.alias["react-dom"] = "preact/compat";
